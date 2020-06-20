63a39f16f3652a1a707178204a96f346043598d9
Ekampreet Kaur <Kaur.ekampreet@outlook.com>
2020-06-20 16:29:38 -0700

    Added Pic Controller

--- EventCatalog/Controllers/PicController.cs
+++ EventCatalog/Controllers/PicController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -13,8 +14,17 @@ namespace EventCatalog.Controllers
     public class PicController : ControllerBase
     {
         private readonly IWebHostEnvironment _env;
-
-        
+        public PicController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+        [HttpGet("{id}")]
         public IActionResult GetImage(int id)
+        {
+            var webRoot = _env.WebRootPath;
+            var path = Path.Combine($"{webRoot}/Pics/", $"Pic{id}.jpg");
+            var buffer = System.IO.File.ReadAllBytes(path);
+            return File(buffer, "image/jpeg");
+        }
     }
 }
